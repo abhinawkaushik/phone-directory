8651be0d154cfc9574d645d0455ce33ef346f33e
Abhinaw Kaushik <abhinaw@live.in>
2021-04-17 10:18:14 +0530

    Passed heading to Header component as props

--- src/App.js
+++ src/App.js
@@ -3,42 +3,38 @@ import './App.css'
 function App() {
   let subscribers = [
     {
-      id:1,
-      name:"Abhinaw",
-      phone:"9999999999"
+      id: 1,
+      name: "Abhinaw",
+      phone: "9999999999"
     },
     {
-      id:2,
-      name:"Kiran",
-      phone:"88888888"
+      id: 2,
+      name: "Kiran",
+      phone: "88888888"
     }
   ]
   return (
-    
+
     <div>
-        <Header />
-        <div className="component-body-container">
-          <button className="custom-btn add-btn">Add</button>
+      <Header heading="Phone Directory" />
+      <div className="component-body-container">
+        <button className="custom-btn add-btn">Add</button>
 
-          <div className="grid-container heading-container">
-            <span className="grid-item name-heading">Name</span>
-            <span className="grid-item phone-heading">Phone</span>
-          </div>
-
-          {
-            subscribers.map(sub=>{
-              return <div key={sub.id} className="grid-container">
-              <span className="grid-item">{sub.name}</span>
-              <span className="grid-item">{sub.phone}</span>
-              <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
-                </span>
-            </div>
-            })
-          }          
-
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
         </div>
+        {subscribers.map(sub => {
+          return <div key={sub.id} className="grid-container">
+            <span className="grid-item">{sub.name}</span>
+            <span className="grid-item">{sub.phone}</span>
+            <span className="grid-item action-btn-container">
+              <button className="custom-btn delete-btn">Delete</button>
+            </span>
+          </div>
+        })}
       </div>
+    </div>
   );
 }
 
